cbe517921233b56895d65487cc7034b0f2e26cae
buffosens <buffo@luppis.de>
2020-03-10 13:26:56 +0100

    - update

---
+++ src/variables_mutability.rs
@@ -0,0 +1,29 @@
+pub fn process()
+{
+    const MAX_POINTS: u32 = 100_000;
+
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let y = 5;
+
+    let y = y + 1;
+
+    let y = y * 2;
+
+    println!("The value of y is: {}", y);
+
+    let mut spaces = "   ";
+    let spaces = spaces.len();
+
+    /*
+    let mut spaces = "   ";
+    spaces = spaces.len();
+    */
+
+    // Error:  not allowed to mutate a variable’s type
+}
