b96cc02b762f7755f79d4f8125efc1bd5dc12403
buffosens <buffo@luppis.de>
2020-03-10 13:04:08 +0100

    - update

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -8,25 +8,33 @@ pub fn process()
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // let -> new variable (let foo = bar), immutable by default
-    // mut -> mutable
-    let mut guess = String::new();
+        // let -> new variable (let foo = bar), immutable by default
+        // mut -> mutable
+        let mut guess = String::new();
 
-    // note the &mut as mutable reference
-    std::io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        // note the &mut as mutable reference
+        std::io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // trim method on a String instance will eliminate any whitespace at the beginning and end
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        // trim method on a String instance will eliminate any whitespace at the beginning and end
+        let guess: u32 = match guess.trim().parse()
+        {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
